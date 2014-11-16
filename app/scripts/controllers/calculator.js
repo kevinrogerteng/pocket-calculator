a35db3aa6c0b327521da453e7aff689c7b669f13
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-11-15 21:45:40 -0800

    got logic down for plus

--- app/scripts/controllers/calculator.js
+++ app/scripts/controllers/calculator.js
@@ -2,14 +2,54 @@
 
 angular.module('pocketCalculatorApp')
   .controller('CaculatorCtrl', function ($scope) {
-    $scope.value = "";
+
+    $scope.value = '';
+    $scope.prevNumber = '';
+    $scope.operation = '';
+    $scope.prevBool = false;
+    $scope.point = false;
+    $scope.operBool = false;
 
     $scope.clearPress = function(){
-      console.log("cleared everything");
-    }
+      console.log('cleared everything');
+    };
 
     $scope.pressValue = function(value){
-      $scope.value = $scope.value + value;
-      console.log($scope.value);
+      if($scope.operBool === true){
+        $scope.value = value;
+      } else {
+        addValue(value);
+      }
+    };
+
+    function addValue(number){
+      $scope.value = $scope.value + number;
     }
+
+    $scope.pressDot = function(dot){
+      if(dot === '.' && $scope.point === false && $scope.value.length < 18){
+        $scope.point = true;
+        $scope.value = $scope.value + dot;
+      }
+    };
+
+    $scope.pressOperations = function(operation){
+
+      if($scope.prevBool === false && $scope.operBool === false){
+        $scope.prevBool = true;
+        $scope.operBool = true;
+        $scope.prevNumber = $scope.value;
+        $scope.operation = operation;
+      } else {
+        var intValue = parseInt($scope.prevNumber) + parseInt($scope.value);
+        $scope.value = intValue.toString();
+        $scope.prevNumber = $scope.value;
+      }
+    };
+
+    $scope.pressEqual = function(){
+      // do somthing
+    };
+
+    // todo
 });
